aaa980d635a6d107feb8182daba1334cadf13f50
Andrew Turner <at@tekn0t.net>
2010-12-13 15:36:16 +0000

    Initial project import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryTestapp::Application.config.secret_token = '9eca28d525645cb78ca30f95540a21e52e3c5840b6d6755ca3501f644554c953573ca4bae3edd7c05fdf05ec6b903c62f620d1cd15a11638805b990f08206c8d'
